e0c09c9a7199d245bfe765c83ba92c05aeca57f3
Annelise <annelzz26@gmail.com>
2020-11-13 21:02:02 -0800

    commit tree 96169bd0610ddcb96228c07f3fc530fb4ad0df75

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+    return false if number <= 1
+    (2..(number - 1)).each do |x|
+        return false if number % x == 0
+    end
+    true
+end
+    
+# def prime?(num)
+#     n = 2
+#     while n < num
+#       return false if num % n == 0
+#       n += 1
+#     end
+#     true
+#   end
+
+  
